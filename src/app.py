4ef99cacb460d067f3c8058b940c1e246483047f
Ethan Matthews <matthewsethan43@gmail.com>
2026-04-10 15:39:44 +1200

    first fetching function working

--- src/app.py
+++ src/app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+import pandas as pd
+from db import client
+
+df = pd.DataFrame(client.fetch_mention_data("AIXI"))
+
+st.dataframe(df)
 
--- src/db/client.py
+++ src/db/client.py
@@ -11,10 +11,26 @@ key = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(url, key)
 
-def fetch_mention_data(ticker: str, hours: int = 12):
+
+def fetch_mention_data(ticker: str, hours: int = 12) -> pd.DataFrame:
+    """Fetch all columns from mentions table for specified ticker and timeframe.
+
+    Args:
+        ticker (str): The ticker to be searched for.
+        hours (int, optional): How many hours to lookback. Supabase inposes limit of 1000 items. Defaults to 12 hours.
+
+    Returns:
+        Pandas Dataframe: The data in the form of a dataframe.
+    """
     end_time = (datetime.now(timezone.utc) - timedelta(hours)).isoformat()
 
-    response = supabase.table("mentions").select("*").eq("ticker", ticker) \
-        .gte("timestamp", end_time).order("timestamp", desc=True).execute()
-    
-    return pd.DataFrame(response)
+    response = (
+        supabase.table("mentions")
+        .select("*")
+        .eq("ticker", ticker)
+        .gte("timestamp", end_time)
+        .order("timestamp", desc=True)
+        .execute()
+    )
+
+    return pd.DataFrame(response.data)
